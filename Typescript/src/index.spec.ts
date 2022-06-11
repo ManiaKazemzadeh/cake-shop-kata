b29fca7972c6c84fe8522b30d4d55f0dbf55a0ce
ManiaKazemzadeh <mania.kazemzadeh@cazoo.co.uk>
2022-06-11 14:11:47 +0100

    IDE setup and pass the first test

--- Typescript/src/index.spec.ts
+++ Typescript/src/index.spec.ts
@@ -1,5 +1,14 @@
-import { order } from ".";
+import { Temporal } from "temporal-polyfill";
+
+import { orderCake } from ".";
+
+const monday = Temporal.PlainDateTime.from("2022-06-06T08:00:00.000");
+const wednesday = Temporal.PlainDateTime.from("2022-06-08T08:00:00.000");
 
 test("A small cake, ordered on Monday, is delivered on Wednesday", () => {
-  const result = order("small");
+  console.log({ monday: JSON.stringify(monday) });
+  console.log({ wednesday: JSON.stringify(wednesday) });
+  const result = orderCake("small", monday);
+
+  expect(result).toEqual(wednesday.toPlainDate());
 });
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -1,5 +1,13 @@
+import { Temporal } from "temporal-polyfill";
+
 type Size = "small" | "big";
 
-export function order(size: Size) {
-  throw new Error("Not implemented");
+export function orderCake(
+  size: Size,
+  orderTime: Temporal.PlainDateTime
+): Temporal.PlainDate {
+  console.log({ ordertime: JSON.stringify(orderTime) });
+  const result = orderTime.add(new Temporal.Duration(0, 0, 0, 2)).toPlainDate();
+  console.log({ result: JSON.stringify(result) });
+  return result;
 }
