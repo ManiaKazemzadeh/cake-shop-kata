fa0fec089bb4a66acf648a0a22ee9428114fe4d6
ManiaKazemzadeh <mania.kazemzadeh@cazoo.co.uk>
2022-06-11 14:32:20 +0100

    default lead time for big cakes + fix name in package.json

--- Typescript/src/index.spec.ts
+++ Typescript/src/index.spec.ts
@@ -4,11 +4,20 @@ import { orderCake } from ".";
 
 const monday = Temporal.PlainDateTime.from("2022-06-06T08:00:00.000");
 const wednesday = Temporal.PlainDateTime.from("2022-06-08T08:00:00.000");
+const thursday = Temporal.PlainDateTime.from("2022-06-09T08:00:00.000");
 
 test("A small cake, ordered on Monday, is delivered on Wednesday", () => {
-  console.log({ monday: JSON.stringify(monday) });
-  console.log({ wednesday: JSON.stringify(wednesday) });
   const result = orderCake("small", monday);
 
-  expect(result).toEqual(wednesday.toPlainDate());
+  expect(result.toLocaleString()).toEqual(
+    wednesday.toPlainDate().toLocaleString()
+  );
+});
+
+test("A big cake, ordered on Monday, is delivered on Thursday", () => {
+  const result = orderCake("big", monday);
+
+  expect(result.toLocaleString()).toEqual(
+    thursday.toPlainDate().toLocaleString()
+  );
 });
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -2,12 +2,21 @@ import { Temporal } from "temporal-polyfill";
 
 type Size = "small" | "big";
 
+function addDays(date: Temporal.PlainDateTime, days: number) {
+  return date.add(new Temporal.Duration(0, 0, 0, days));
+}
+
+function getLeadTime(size: Size): number {
+  return size === "small" ? 2 : 3;
+}
+
 export function orderCake(
   size: Size,
   orderTime: Temporal.PlainDateTime
 ): Temporal.PlainDate {
-  console.log({ ordertime: JSON.stringify(orderTime) });
-  const result = orderTime.add(new Temporal.Duration(0, 0, 0, 2)).toPlainDate();
+  const leadTime = getLeadTime(size);
+  console.log({ orderTime: JSON.stringify(orderTime) });
+  const result = addDays(orderTime, leadTime).toPlainDate();
   console.log({ result: JSON.stringify(result) });
   return result;
 }
